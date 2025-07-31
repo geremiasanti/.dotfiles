99fcc37adbe5ba8f0155cbd75045942cb77d37ac
Geremia Santi <geremiasanti.dev@gmail.com>
2025-07-31 14:43:26 +0200

    lualine sections

--- .config/nvim/lua/plugins/lualine.lua
+++ .config/nvim/lua/plugins/lualine.lua
@@ -2,6 +2,12 @@ return {
     'nvim-lualine/lualine.nvim',
     dependencies = { 'nvim-tree/nvim-web-devicons' },
     config = function()
-        require('lualine').setup()
+        require('lualine').setup({
+          sections = {
+            lualine_b = {'branch', 'filename'},
+            lualine_c = {'diff'},
+            lualine_x = {'diagnostics'},
+          },
+        })
     end
 }
